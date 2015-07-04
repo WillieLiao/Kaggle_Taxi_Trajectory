5a82355465d09102681fc17abe30b334f1629c9e
Willie Liao <will.liao932@gmail.com>
2015-07-04 01:03:18 -0700

    Change r2 back to int.

--- read_data.R
+++ read_data.R
@@ -19,7 +19,7 @@ poly <- train[, unlist(fromJSON(polyline)), by=id]
 poly[, r:=(1:.N)%%2]
 poly[, r2:=(1:.N)]
 setkey(poly, id)
-poly[r==0, r2:=r2-1]
+poly[r==0, r2:=r2-1L]
 poly <- merge(poly[r==1, list(id, r2, lon=V1)], poly[r==0, list(id, r2, lat=V1)], by=c('id', 'r2'), all=T)
 poly[, r2:=NULL]
 
